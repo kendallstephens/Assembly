bed83c0ec275bbbc69f2527a5d8b45f18ef96750
kendallstephens <kendoll.stephens@gmail.com>
2021-02-14 16:21:37 -0800

    small css update

--- components/prebuilt/Row.jsx
+++ components/prebuilt/Row.jsx
@@ -6,7 +6,7 @@ const Row = styled.div`
   box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08),
     inset 0 1px 0 #829fff;
   border-radius: 4px;
-  background-color: #f7cac9;
+  background-color: #F0F8FF;
   position: relative;
 `;
 
