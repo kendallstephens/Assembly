ca21470745c2211385535d9644bf35cb30e470a5
kendallstephens <kendoll.stephens@gmail.com>
2021-02-21 16:39:22 -0800

    added login form

---
+++ components/LoginForm.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {Form, Grid, Button, Message, Segment} from 'semantic-ui-react'
+import {NavLink} from 'react-router-dom'
+
+
+class LoginForm extends React.Component {
+
+  state = {
+      email: '',
+      password: ''
+    }
+  
+    handleSubmit = (e) => {
+      e.preventDefault()
+      this.props.handleSubmit(this.state)
+    }
+  
+    handleChange = (e) => {
+      let {name, value} = e.target
+      this.setState({
+        [name]: value
+      })
+    }
+
+
+
+render(){
+  const {email, password} = this.state
+  const {handleSubmit, handleChange} = this
+
+return (
+
+  <div>
+    
+    <Grid textAlign='center' verticalAlign='middle'>
+      <Grid.Column style={{maxWidth: 450}}>
+        
+        <Form className = 'login-form' onSubmit={handleSubmit} size = 'small'>
+        <h3>Login</h3>
+          <Segment stacked>
+        <Form.Input  fluid icon='user' iconPosition='left' type='text' name='email' placeholder='E-mail address' value={email} onChange={handleChange}/><br />
+        <Form.Input fluid icon='lock' iconPosition='left' type='password' name='password' placeholder='Password' value={password} onChange={handleChange}/><br />
+        <Button color='black' fluid size='large'>Login</Button>  
+          </Segment>
+       </Form>
+       <Message>
+        First time here? <NavLink to = '/signup'>Sign Up</NavLink>
+      </Message>
+      </Grid.Column>
+    </Grid>
+  </div>
+      )
+  }
+}
+
+export default LoginForm;
