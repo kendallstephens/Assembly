5eced8846e7c70d1680a023e0903ab015075e23d
kendallstephens <kendoll.stephens@gmail.com>
2021-03-14 20:06:15 -0700

    martini kit added

--- pages/api/cocktail_kit_data.js
+++ pages/api/cocktail_kit_data.js
@@ -1,10 +1,19 @@
 
 const cocktailKits = [
-    { name: 'Negroni',
+    { 
+    name: 'Negroni',
     price: 60,
     ingredients: 'Gin, Campari, Sweet Vermouth' ,
     description: 'This is the classic Italian way to open a meal: the Negroni, a bittersweet palate cleanser that makes way for the evening’s delights. Adding a bottle of Campari to the liquor cabinet opens up a world of cocktail possibilities. In any case, drink this cold. This version came to us from Jim Meehan, a bartender and an owner of Please Dont Tell, a speakeasy in the East Village.',
     image: 'https://images.unsplash.com/photo-1587315118384-dcd4f6ebb90e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fG5lZ3Jvbml8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'  
+    },
+    {
+        name: 'Martini',
+        price: 60,
+        ingredients: 'Gin, Noilly Prat dry vermouth, Lemon' ,
+        description: 'Even if you’re thinking to yourself, but I already know how to make a martini, you may want to keep reading anyway. This martini recipe is proof that sometimes the simplest things are actually the hardest to master. It might only need three ingredients—gin, vermouth, and a lemon twist—but fewer variables means that each one is very important to the final result. Technique is everything here.',
+        image: 'https://images.unsplash.com/photo-1575023782549-62ca0d244b39?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hcnRpbml8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'  
+
     }
 ]
 
