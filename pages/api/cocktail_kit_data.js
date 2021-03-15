a3103608280e19b8fd4de07dc1f09dc046dd1536
kendallstephens <kendoll.stephens@gmail.com>
2021-03-14 20:12:19 -0700

    recipe added

--- pages/api/cocktail_kit_data.js
+++ pages/api/cocktail_kit_data.js
@@ -21,6 +21,13 @@ const cocktailKits = [
         description: 'Inspired by the quintessential version served at Curra’s Grill in Austin. Trust us, it’s good!',
         image: 'https://images.unsplash.com/photo-1541976076758-347942db1975?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bWFyZ2FyaXRhfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'  
     }, 
+    {
+        name: 'Amaro Cocktail',
+        price: 60,
+        ingredients: 'Rye,  Amaro Montenegro, Egg White' ,
+        description: 'Traditionally, orgeat is almond syrup, but Joseph Stinchcomb, beverage director at Saint Leo in Oxford, MS, has made it with everything from walnuts to pistachios. “It gives a beautiful toasted nut flavor to a cocktail,” he says. “Here I used pecans because they remind me of the family pecan tree at my grandparents’ house in Fayetteville, GA. Roasted pecans. Warm baking spices. Citrusy, caramelly Amaro Montenegro. A bit of spicy rye whiskey. They all work well together to create a cocktail that feels toasty. That’s what I love in a winter drink.” A second shake with an egg white creates a velvety texture and foamy top.',
+        image: 'https://assets.bonappetit.com/photos/5fb7e7445d6dedbf765ba756/1:1/w_2560%2Cc_limit/1220-drinks-Amaro-Cocktail.jpg'  
+    }, 
     
 ]
 
