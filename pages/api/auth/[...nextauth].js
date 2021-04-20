947bfc43b6a4e0cbe99450d2a2ea38d5d94dc649
kendallstephens <kendoll.stephens@gmail.com>
2021-04-20 16:22:08 -0700

    small auth update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,10 @@ import Providers from 'next-auth/providers'
 const options = {
     providers: [
         Providers.Google({
+            
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
+            authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
         }),
     ],
 }
